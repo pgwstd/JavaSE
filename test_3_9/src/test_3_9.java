5bc23e42f09c0206ff66626c977b794dfae8221a
pgwstr <2712278005@qq.com>
2022-03-10 13:16:07 +0800

    作业更新

--- test_3_9/src/test_3_9.java
+++ test_3_9/src/test_3_9.java
@@ -1,6 +1,6 @@
 import java.util.Arrays;
 public class test_3_9 {
-        public static void main (String args[ ]){
+        public static void main (String[] args){
             char ch='研',zifu=0;
             int p=22920,count=5,position=0;
             System.out.printf("输出%d个石字傍的汉字::\n",count);
@@ -10,7 +10,6 @@ public class test_3_9 {
             }
             System.out.printf("\n输出%d个女字傍的汉字::\n",count);
             for(int n=p;n<=p+count;n++) {
-
                 System.out.printf("%c(%d)",(char)n,n);
             }
         }
