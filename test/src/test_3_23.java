13ad13f1847204260273d69629a04ff0a7a3f60a
pgwstr <2712278005@qq.com>
2022-03-23 18:07:28 +0800

    作业更新

--- test/src/test_3_23.java
+++ test/src/test_3_23.java
@@ -1,3 +1,21 @@
+//符号倒序
 public class test_3_23 {
-
+    public static void main(String args[]) {
+        int i = 0;
+        int j = 0;
+        System.out.println("原来的样子:");
+        for (i = 0; i < 9; i++) {
+            for (j = 9; j >i ; j--) {
+                   System.out.print("*");
+            }
+            System.out.println("\n");
+        }
+        System.out.println("改变后的样子:");
+        for (i = 0; i < 9; i++) {
+            for (j = 0; j <i ; j++) {
+                System.out.print("*");
+            }
+            System.out.println("\n");
+        }
+    }
 }
--- test/src/test_3_23_2.java
+++ test/src/test_3_23_2.java
@@ -1,2 +1,3 @@
+//3阶魔方矩阵
 public class test_3_23_2 {
 }
