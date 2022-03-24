c07f0e52d5178a54215021efd4699194aafdd15f
pgwstr <2712278005@qq.com>
2022-03-24 08:22:09 +0800

    作业更新

--- test/src/test_3_23_3.java
+++ test/src/test_3_23_3.java
@@ -1,50 +1,50 @@
 //杨辉三角
 public class test_3_23_3 {
-//        public static void main(String[] args) {
-//
-//            int[][] arr=new int[10][10];
-//            for(int i=0;i<arr.length; i++) {
-//
-//                for(int j=0;j<=i;j++) {
-//
-//                    if(j==0||j==i) {
-//                        arr[i][j]=1;
-//                    }else {
-//                        arr[i][j]=arr[i-1][j-1]+arr[i-1][j];
-//                    }
-//
-//                    System.out.print(arr[i][j]+"\t");
-//                }
-//                System.out.println();
-//            }
-//        }
-//    }
-    public static void main(String[] args) {
-        int[][] arr=new int[10][10];
-        for(int i=0;i<arr.length;i++) {
-            arr[i]=new int[i+1];
-        }
-        for(int i=0;i<arr.length;i++) {
-            for(int j=0;j<arr[i].length;j++) {
-                if(j==0 || i==j) {
-                    arr[i][j]=1;
-                }else {
-                    arr[i][j]=arr[i-1][j]+arr[i-1][j-1];
+        public static void main(String[] args) {
+
+            int[][] arr=new int[10][10];
+            for(int i=0;i<arr.length; i++) {
+
+                for(int j=0;j<=i;j++) {
+
+                    if(j==0||j==i) {
+                        arr[i][j]=1;
+                    }else {
+                        arr[i][j]=arr[i-1][j-1]+arr[i-1][j];
+                    }
+
+                    System.out.print(arr[i][j]+"\t");
                 }
+                System.out.println();
             }
-        }
-        for(int i=0;i<arr.length;i++) {
-            int num=(arr.length-i)/2;
-            for(int k=0;k<=num;k++) {
-                System.out.print("\t");
-            }
-            for(int j=0;j<arr[i].length;j++) {
-                System.out.print(arr[i][j]+"\t");
-            }
-            System.out.println();
         }
     }
-}
+//    public static void main(String[] args) {
+//        int[][] arr=new int[10][10];
+//        for(int i=0;i<arr.length;i++) {
+//            arr[i]=new int[i+1];
+//        }
+//        for(int i=0;i<arr.length;i++) {
+//            for(int j=0;j<arr[i].length;j++) {
+//                if(j==0 || i==j) {
+//                    arr[i][j]=1;
+//                }else {
+//                    arr[i][j]=arr[i-1][j]+arr[i-1][j-1];
+//                }
+//            }
+//        }
+//        for(int i=0;i<arr.length;i++) {
+//            int num=(arr.length-i)/2;
+//            for(int k=0;k<=num;k++) {
+//                System.out.print("\t");
+//            }
+//            for(int j=0;j<arr[i].length;j++) {
+//                System.out.print(arr[i][j]+"\t");
+//            }
+//            System.out.println();
+//        }
+//    }
+//}
 
 
 
---
+++ test/src/test_3_24_2.java
@@ -0,0 +1,58 @@
+import java.util.Random;
+import java.util.Scanner;
+    public class test_3_24_2 {
+
+        public static int getRanNum(){
+
+            Random random = new Random();
+
+            return random.nextInt(100);
+
+        }
+
+        public static boolean guess(Scanner scanner,int toGuess){
+            System.out.println("请输入要猜的数(1-100)：");
+
+            int num = scanner.nextInt();
+
+            if ( num < toGuess){
+                System.out.println("小了...");
+
+                return false;
+
+            } else if ( num > toGuess){
+                System.out.println("大了...");
+
+                return false;
+
+            } else {
+                System.out.println("恭喜你，猜对了！！");
+
+                return true;
+
+            }
+
+        }
+
+        public static void startGame(Scanner scanner){
+            int toGuess = getRanNum();
+
+            while( true ){
+                if (guess(scanner,toGuess)){
+                    break;
+
+                }
+
+            }
+
+        }
+
+        public static void main(String[] args) {
+            Scanner scanner = new Scanner(System.in);
+
+            startGame(scanner);
+
+        }
+
+    }
+
