fe582a20f3ae67cf95e1f8e8ca48eb3ae70c9406
pgwstr <2712278005@qq.com>
2022-03-25 16:49:24 +0800

    作业更新

--- test/src/test_3_24_5.java
+++ test/src/test_3_24_5.java
@@ -1,6 +1,14 @@
+import java.util.Scanner;
+
 public class test_3_24_5 {
     public static void main(String args[]){
-        int []arr;
+        System.out.println("请定义数组的长度");
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int []arr=new int[n];
+        for (int i=0;i<n;i++){
+            sc.nextInt(arr[i]);
+        }
 
     }
 }
