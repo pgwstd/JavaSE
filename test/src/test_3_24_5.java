b2054aa4898b38dbb8cdabb44001618e6579c37e
pgwstr <2712278005@qq.com>
2022-03-25 21:52:09 +0800

    作业更新

--- test/src/test_3_24_5.java
+++ test/src/test_3_24_5.java
@@ -1,22 +1,18 @@
 //通过键盘输入一个数组，遍历输出它的值，再输出数组中最大值的索引号
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+
 import java.util.Scanner;
 
 public class test_3_24_5 {
-    public static void main(String args[]) throws IOException {
+    public static void main(String args[])  {
         System.out.println("请定义数组的长度:");
         //输入定义长度
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         System.out.println("请输入数组:");
         int[] arr = new int[n];
-        int tmp=0;
         int max=0;
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         for (int i = 0; i < n; i++) {
-            arr[i] = Integer.parseInt(br.readLine());
+            arr[i]=sc.nextInt();
 
         }
         System.out.println("遍历已输入的数组:");
