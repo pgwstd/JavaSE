ff85724c3687b64cab756a58d3e435f55427036f
pgwstr <2712278005@qq.com>
2022-03-25 23:09:34 +0800

    作业更新

--- test/src/test_3_25_2.java
+++ test/src/test_3_25_2.java
@@ -2,11 +2,15 @@
 public class test_3_25_2 {
     public static void main(String[] args) {
         int [][]arr=new int[7][7];
-        for (int i=1;i<10;i++){
+        int n=1;
+        for (int i=1;i<7;i++){
             for (int j=1;j<i;j++){
-                
+                   arr[7/2][7/2]=n;
+                   n++;
+
             }
         }
+        System.out.println(arr);
     }
 }
 
