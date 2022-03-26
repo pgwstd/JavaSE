f1ba802179b95ebf03c3bf7628c9b029a270987f
pgwstr <2712278005@qq.com>
2022-03-26 13:07:14 +0800

    作业更新

--- test/src/test_3_26.java
+++ test/src/test_3_26.java
@@ -1,4 +1,11 @@
-//计算两具个数非零整数的最大公约数和最小公倍数
-public class test_3_26 {
+import java.util.Scanner;
 
+//计算两个非零整数的最大公约数和最小公倍数
+public class test_3_26 {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        sc.nextInt();
+        for (int i=0;i<2;i++) {
+        }
+    }
 }
--- test/src/test_3_26_2.java
+++ test/src/test_3_26_2.java
@@ -2,11 +2,18 @@
 public class test_3_26_2 {
     public static void main(String[] args) {
         int n=1;
+        int m=0;
         int sum=0;
-         for (int i=1;i<3;i++){
-             sum=1;
-             sum+=i;
+         for (int i=1;i<=10;i++){
+                 m = n *= i;
+                 sum += m;
+                 if (sum>9999) {
+                     i=i-1;
+                     System.out.println("最大的n是："+i);
+                     break;
+             }
+
          }
-         System.out.println(sum);
+
     }
 }
