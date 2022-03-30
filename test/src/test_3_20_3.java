75d0d51dac5d88b6c933cb985ff35a3ea2b45bc5
pgwstr <2712278005@qq.com>
2022-03-30 23:52:38 +0800

    作业更新

--- test/src/test_3_20_3.java
+++ test/src/test_3_20_3.java
@@ -6,15 +6,16 @@ public class test_3_20_3 {
         float n = 1.0f;
         float sum = 0;
         int k = 1;
-        while (true) {
             for (i = 2; i <= 20; i++) {
                 sum += n;
-                n = n /(n * i);
-
+                n = n *(n / i);
             }
-
             System.out.println(sum);
-            break;
+            while (k<=20){
+                sum += n;
+                n = n *(n / i);
+                k++;
+            }
+        System.out.println(sum);
         }
     }
-}
