e32ffadbef495db7703de63ea91bfe9ee3d90289
pgwstr <2712278005@qq.com>
2022-03-30 22:29:09 +0800

    作业更新

---
+++ test/src/test_3_30.java
@@ -0,0 +1,21 @@
+//输出俄文字母表
+public class test_3_30 {
+    public static void main(String[] args) {
+        for (char i=1040;i<=1071;i++){
+            System.out.printf((char)i+" ");
+        }
+//        int startPosition=0,endPosition=0;
+//        char cStart='А',cEnd='Я';
+//        startPosition=(int)cStart;
+//        endPosition=(int)cEnd;
+//        System.out.println("俄文字母共有:"+(endPosition-startPosition+1)+"个");
+//        for(int i=startPosition;i<=endPosition;i++)
+//        {
+//            char c='\0';
+//            c=(char)i;
+//            System.out.print(" "+c);
+//            System.out.print(" "+(int)c);
+//        }
+    }
+    }
+
