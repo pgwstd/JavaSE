1e87d6ea1183c28cb43bbf3bef6ab79bcf32aecb
pgwstr <2712278005@qq.com>
2022-04-03 19:08:03 +0800

    作业更新

--- test/src/triangle.java
+++ test/src/triangle.java
@@ -1,12 +1,14 @@
 public class triangle {
-    int a0, b0, c0, getEdge, zhouchang;
+    private int a0,b0,c0;
+    private int maxEdge;
+    private int zhouchang;
 
-    public int geta0() {
+    public int getA0() {
         return a0;
     }
 
-    public int getZhouchang(int a0, int b0, int c0) {
-        zhouchang += a0 + b0 + c0;
+    public int getZhouchang(int a0,int b0,int c0) {
+        zhouchang+=a0+b0+c0;
         return zhouchang;
     }
 
@@ -33,30 +35,28 @@ public class triangle {
     public void setC0(int c0) {
         this.c0 = c0;
     }
-
-    public void perimeter(int a0, int b0, int c0) {
-        getZhouchang(a0, b0, c0);
+    public void perimeter(int a0,int b0,int c0){
+        getZhouchang(a0,b0,c0);
         System.out.println(zhouchang);
     }
-
-    public void Isosceles(int a0, int b0, int c0) {
-        if (a0 == b0 && a0 == c0) {
-            System.out.println("是等边三角形");
-        } else if (a0 == b0 || b0 == c0) {
+    public void Isosceles(int a0,int b0,int c0){
+        if (a0==b0&&a0==c0){
+            System.out.println( "是等边三角形");
+        }else if(a0==b0||b0==c0){
             System.out.println("是等腰三角形");
-        } else {
+        }
+        else {
             System.out.println("不是等腰三角形也不是等边三角形");
         }
     }
-
-    public void Longest(int a0, int b0, int c0) {
-        int max = a0;
-        if (max < b0) {
-            max = b0;
+    public void Longest(int a0,int b0,int c0){
+        int max=a0;
+        if(max<b0){
+            max=b0;
         }
-        if (max < c0) {
-            max = c0;
+        if (max<c0){
+            max=c0;
         }
-        System.out.println("最长边是" + max);
+        System.out.println("最长边是"+max);
     }
 }
