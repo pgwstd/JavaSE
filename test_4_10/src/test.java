73dfd48303f4fb61dabd547c00287921f1535a20
pgwstr <2712278005@qq.com>
2022-04-10 19:02:15 +0800

    作业更新

--- test_4_10/src/test.java
+++ test_4_10/src/test.java
@@ -9,4 +9,10 @@ public class test {
     public static void main(String[] args) {
         System.out.println("Hello World!");
     }
+    class A{
+        public static void main(String[] args) {
+
+            System.out.println("A");
+        }
+    }
 }
