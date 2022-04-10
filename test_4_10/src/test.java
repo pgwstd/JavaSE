cafed5c59bed3862828a96602edc6a3dcc99c3a2
pgwstr <2712278005@qq.com>
2022-04-10 18:54:40 +0800

    作业更新

---
+++ test_4_10/src/test.java
@@ -0,0 +1,12 @@
+//
+// Created by 潘光伟 on 2022/4/10.
+//
+
+
+import javafx.beans.property.SimpleLongProperty;
+
+public class test {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
