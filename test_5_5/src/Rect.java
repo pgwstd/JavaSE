d02c28cc9762bfd1d46e624f0b52f4d6a2daaf02
pgwstr <2712278005@qq.com>
2022-05-05 18:51:44 +0800

    作业更新

---
+++ test_5_5/src/Rect.java
@@ -0,0 +1,18 @@
+import test.Geometry;
+
+/**
+ * Created by 潘光伟 on 2022/5/5 18:50.
+ */
+public  class Rect extends Geometry {
+    double a,b;
+    Rect(double a,double b) {
+        this.a = a;
+        this.b = b;
+    }
+
+    //重写 getArea()方法
+    @Override
+    public double getArea() {
+        return a*b;
+    }
+}
