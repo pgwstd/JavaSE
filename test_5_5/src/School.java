a91c1a08db824275ff83f6ee6c1de61fa8e0476c
pgwstr <2712278005@qq.com>
2022-05-05 19:10:54 +0800

    作业更新

--- test_5_5/src/School.java
+++ test_5_5/src/School.java
@@ -5,7 +5,7 @@ public class School implements CompurerAverage {
    //重写public double average(double x[])方法,返回数组x[]的元素的算术平均
     @Override
     public double average(double x[]){
-        return 11;
+        return average(x);
     }
 }
 
