5786b9373a759e06adaf2302850fec0499fcfd7c
pgwstr <2712278005@qq.com>
2022-05-19 09:07:22 +0800

    作业更新

--- test_5_19/src/Avg.java
+++ test_5_19/src/Avg.java
@@ -2,14 +2,19 @@
  * Created by 潘光伟 on 2022/5/19 08:34.
  */
 //解析出“数据结构89分，离散数学92分，Java编程86分，组成原理76分”的成绩，计算并输出平均分
+    import java.util.regex.*;
 public class Avg {
     public static void main(String[] args) {
-        String str = "数据结构89分,离散数学92分,Java编程86分,组成原理76分";
-        String[] strings = str.split(",");
-        int sum = 0;
-        for (String s : strings) {
-            sum += Integer.parseInt(s.substring(0, s.length() - 1));
+        String str = "数据结构89分，离散数学92分，Java编程86分，组成原理76分";
+        Pattern p = Pattern.compile("\\d+(,\\d{3})*");
+        Matcher m = p.matcher(str);
+        int sum=0;
+        int count=0;
+        while (m.find()) {
+            String s = m.group();
+            sum+=Integer.parseInt(s);
+            count++;
         }
-        System.out.println(sum / strings.length);
+        System.out.println("平均分为："+sum/count);
     }
 }
---
+++ test_5_19/src/Test.java
@@ -0,0 +1,20 @@
+/**
+ * Created by 潘光伟 on 2022/5/19 08:39.
+ */
+//从键盘输入一个纯数字的字符串，转成整数后，再转成二进制数输出，并输出该二进制数包含1的个数
+import java.util.Scanner;
+public class Test {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String s = sc.next();
+        int num = Integer.parseInt(s);
+        String s1 = Integer.toBinaryString(num);
+        int count = 0;
+        for (int i = 0; i < s1.length(); i++) {
+            if (s1.charAt(i) == '1') {
+                count++;
+            }
+        }
+        System.out.println(count);
+    }
+}
