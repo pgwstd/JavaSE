3304de0daa5918d8d64e2b3d27256ec8db8c7a62
panguangwei <2712278005@qq.com>
2022-06-16 21:37:58 +0800

    作业更新

--- test_6_16/src/test2.java
+++ test_6_16/src/test2.java
@@ -1,22 +1,13 @@
 /**
  * Created by 潘光伟 on 2022/6/16 19:13.
  */
-import java.lang.*;
-import java.io.*;
-public class test2 {
-        public static void main(String arg[]) extends IOException {
-            InputStreamReader inReader=new InputStreamReader(System.in);
-            BufferedReader bfred=new BufferedReader(inReader);
-            FileOutputStream fWrite=new FileOutputStream("test.txt");
-            String rString;
-            do {
-                System.out.println("\n Please enter :");
-                System.out.flush();
-                rString=bStream.readLine();
-                System.out.println("Hello User>This is:");
-                fWrite.write(rString.getBytes());
-                System.out.println("> "+rString);
-            }while(rString.length()!=0);
-        }
 
+public class test2 {
+    public static void main(String[] args) {
+        int a[] = {11, 22, 33, 44, 55, 66, 77, 88, 99};
+        for (int i = 0; i < 9; i++) {
+            if (a[i] % 33 == 0)
+                System.out.println(a[i]);
+        }
     }
+}
---
+++ test_6_16/src/test_6_16.java
@@ -0,0 +1,35 @@
+/**
+ * Created by 潘光伟 on 2022/6/16 21:33.
+ */
+        class IllegalAgeException extends Exception {
+            public String getMessage() {
+                return "Error age";
+            }
+        }
+        class UserTrial {
+            int age;
+            //在下行划线处填上声明抛弃IllegalAgeException异常的语句
+            void setAge(int a) throws IllegalAgeException {
+                if (a>60) //在下行划线处填上抛出IllegalAgeException异常的语句
+                    throw new IllegalAgeException();
+                    age = a;
+            }
+        }
+        class ThrowExample {
+            public static void main(String args[]) {
+                UserTrial values = new UserTrial();
+                try {
+                    values.setAge(75);
+                }
+//在下行空白处填上捕获IllegalAgeException异常的语句
+                catch (IllegalAgeException e) {
+                    System.out.println(e.getMessage());
+
+                    {
+                        System.out.println(e.getMessage());
+                    }
+                }
+
+            }
+
+        }
