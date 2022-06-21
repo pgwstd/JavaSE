4def62530797ce72c5221df520e5bc79826b04a6
panguangwei <2712278005@qq.com>
2022-06-21 18:36:56 +0800

    作业更新

--- test_6_18/src/Example.java
+++ test_6_18/src/Example.java
@@ -12,11 +12,13 @@ public class Example {
         double h = 25.28;
         rect.setWidth(w);
         rect.setHeight(h);
+
         System.out.println("矩形的宽是: " + rect.getWidth()+"高是: " + rect.getHeight());
         System.out.println("矩形的面积:"+rect.getArea());
         System.out.println("更改传参的值");
         w=100;
         h=256;
+
         System.out.println("矩形的宽是:"+rect.getWidth()+"高是:"+rect.getHeight());
     }
 }
