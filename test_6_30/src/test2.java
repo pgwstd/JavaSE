9388960cee829e31a9a9310225b51183efe025d7
pgwstr <2712278005@qq.com>
2022-06-30 13:29:43 +0800

    作业更新

--- test_6_30/src/test2.java
+++ test_6_30/src/test2.java
@@ -6,18 +6,18 @@ public class test2 {
 
     public static void main(String[] args) throws IOException {
         InputStreamReader inReader = new InputStreamReader(System.in);  //创建一个输入流读取器对象
-        BufferedReader bStream = new BufferedReader(inReader);
-        FileOutputStream fWrite = new FileOutputStream("student.txt", true);
-        String rString;
+        BufferedReader bStream = new BufferedReader(inReader);  //创建一个缓冲输入流对象
+        FileOutputStream fWrite = new FileOutputStream("student.txt", true);  //创建一个文件输出流对象，true表示追加写入
+        String rString;  //定义一个字符串变量
         do {
             System.out.println("\n Please enter :");
-            System.out.flush();
+            System.out.flush();  //刷新缓冲区
 
-            rString = bStream.readLine();
+            rString = bStream.readLine();  //读取一行字符串
 
             System.out.println("Hello User>This is:");
-            fWrite.write(rString.getBytes());
-            System.out.println("> " + rString);
+            fWrite.write(rString.getBytes());  //将读取的字符串写入文件
+            System.out.println("> " + rString);   //输出读取的字符串
         } while (rString.length() != 0);
 
     }
