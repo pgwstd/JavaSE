18bb9f1479b8958180e44be73c02d890be23d996
pgwstr <2712278005@qq.com>
2022-07-03 22:12:36 +0800

    作业更新

--- Diploma_Project/src/user.java
+++ Diploma_Project/src/user.java
@@ -1,4 +1,17 @@
 //用户登录
+import java.util.Scanner;
 public class user {
 
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("请输入用户名：");
+        String username = sc.next();
+        System.out.println("请输入密码：");
+        String password = sc.next();
+        if (username.equals("admin") && password.equals("admin")) {
+            System.out.println("登录成功！");
+        } else {
+            System.out.println("登录失败！");
+        }
+    }
 }
