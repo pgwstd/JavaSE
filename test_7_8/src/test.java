069bcb580afc3fdf8b0142bbe0ecc967627fb155
pgwstr <2712278005@qq.com>
2022-07-09 22:24:41 +0800

    作业更新

--- test_7_8/src/test.java
+++ test_7_8/src/test.java
@@ -9,8 +9,12 @@ public class test {
         float f1 = 12.3f;
         double b1 = 123.4;
         long l1=1222354345345343l;
+        int n = 12;
+        int n2 = 5;
+        double d1 =(double) n / n2;
         System.out .println(f1);
         System.out.println(b1);
         System.out.println(l1);
+        System.out.println(d1);
     }
 }
