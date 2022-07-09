307c6030594e0dd925fc90d44da7d0bb4f209310
pgwstr <2712278005@qq.com>
2022-07-09 11:45:40 +0800

    作业更新

--- test_7_8/src/test.java
+++ test_7_8/src/test.java
@@ -1,5 +1,14 @@
+import java.awt.*;
+
 public class test {
     public static void main(String[] args){
-        System.out.println("Hello,world");
+        System.out.println("姓名：张三");
+        System.out.println();
+        System.out.println("性别：男");
+        System.out.println("地址：北流");
+        float f1 = 12.3f;
+        double b1 = 123.4;
+        System.out .println(f1);
+        System.out.println(b1);
     }
 }
