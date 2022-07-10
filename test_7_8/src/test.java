b0a8c59cc1138c7a13701ff55403ad036dcfe4fe
pgwstr <2712278005@qq.com>
2022-07-10 13:56:55 +0800

    作业更新

--- test_7_8/src/test.java
+++ test_7_8/src/test.java
@@ -2,29 +2,34 @@ import java.awt.*;
 
 public class test {
     public static void main(String[] args){
-        System.out.println("姓名：张三");
-        System.out.println();
-        System.out.println("性别：男");
-        System.out.println("地址：北流");
-        float f1 = 12.3f;
-        double b1 = 123.4;
-        long l1=1222354345345343l;
-        int n = 12;
-        int n2 = 5;
-        double d1 =(double) n / n2;
-        int m2 = 12;
-        int n3 = -5;
-        int num = 10;
-        int num2 = 20;
-        num = num ^ num2;
-        num2 = num ^ num2;
-        num = num ^ num2;
-        System.out.println(num);
-        System.out.println(num2);
-        System.out .println(m2 % n3);
-        System.out .println(f1);
-        System.out.println(b1);
-        System.out.println(l1);
-        System.out.println(d1);
+//        System.out.println("姓名：张三");
+//        System.out.println();
+//        System.out.println("性别：男");
+//        System.out.println("地址：北流");
+//        float f1 = 12.3f;
+//        double b1 = 123.4;
+//        long l1=1222354345345343l;
+//        int n = 12;
+//        int n2 = 5;
+//        double d1 =(double) n / n2;
+//        int m2 = 12;
+//        int n3 = -5;
+//        int num = 10;
+//        int num2 = 20;
+//        num = num ^ num2;
+//        num2 = num ^ num2;
+//        num = num ^ num2;
+//        System.out.println(num);
+//        System.out.println(num2);
+//        System.out .println(m2 % n3);
+//        System.out .println(f1);
+//        System.out.println(b1);
+//        System.out.println(l1);
+//        System.out.println(d1);
+          int n1 = 12;
+          int n2 = 30;
+          int n3 = -43;
+          int max = (n1>n2) ? n1:((n2>n3)?n2:n3);
+          System.out.println(max);
     }
 }
