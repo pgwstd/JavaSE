fcd18bba862d76e71b61ef07f454e8f69f9d28d1
pgwstr <2712278005@qq.com>
2022-07-11 18:20:12 +0800

    作业更新

--- test_7_8/src/test.java
+++ test_7_8/src/test.java
@@ -243,6 +243,7 @@ public class test {
             for (int j = 2;j < i;j++){
                 if (i % j == 0){
                     isFlag = false;
+                    break;
                 }
             }
             if (isFlag){
