1fdbb1714af63c0a7b27128bd9d0020905438e13
pgwstr <2712278005@qq.com>
2022-07-16 11:20:38 +0800

    算法练习

---
+++ test_Arithmetic/src/test_3.java
@@ -0,0 +1,20 @@
+/**
+ * 定义一个int类型变量i,
+ * i为由浮点数变量d四舍五入后的整数类型，
+ * 请将转换后的i进行输出
+ */
+
+import java.util.Scanner;
+public class test_3 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        double d= scanner.nextDouble();
+        //write your code here......
+        int i = 0;
+        i = (int)d;
+        if((d - (double)i) >= 0.5 ){   //如果相差大于或等于0.5就加上1
+            i += 1;
+        }
+        System.out.println(i);
+    }
+}
