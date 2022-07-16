655da75e91e70a6ab126128703b42acf5c5d79ce
pgwstr <2712278005@qq.com>
2022-07-16 10:40:17 +0800

    学习中练习的代码

---
+++ test_Arithmetic/src/test.java
@@ -0,0 +1,19 @@
+/**
+ * 设计一个方法，将一个小于2147483647的double类型变量以截断取整方式转化为int类型
+ *
+ */
+
+import java.util.Scanner;
+public class test {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);  //从键盘输入
+        double num = scanner.nextDouble();
+        System.out.println(typeConversion(num));
+    }
+
+    public static int typeConversion(double d) {
+        //write your code here......
+        return (int) d;
+
+    }
+}
