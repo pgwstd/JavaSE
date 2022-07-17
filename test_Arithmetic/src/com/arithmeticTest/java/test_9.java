41e2f03c34bed04e5805e6f9b224fea1808ad399
pgwstr <2712278005@qq.com>
2022-07-17 11:36:54 +0800

    算法练习

--- test_Arithmetic/src/com/arithmeticTest/java/test_9.java
+++ test_Arithmetic/src/com/arithmeticTest/java/test_9.java
@@ -1,4 +1,16 @@
 package com.arithmeticTest.java;
-
+/*
+数列求和
+ */
 public class test_9 {
+    public static void main(String[] args) {
+        long sum = 0;
+        long i = 9;
+        while (i <= 9999999999L){
+            sum += i;
+            i = (i * 10)+9;
+        }
+
+        System.out.println(sum);
+    }
 }
