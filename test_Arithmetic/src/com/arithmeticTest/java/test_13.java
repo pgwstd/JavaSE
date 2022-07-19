b51e29f750b51bb6f44c28559202e8508f0c1ec5
pgwstr <2712278005@qq.com>
2022-07-19 13:34:55 +0800

    算法练习

--- test_Arithmetic/src/com/arithmeticTest/java/test_13.java
+++ test_Arithmetic/src/com/arithmeticTest/java/test_13.java
@@ -1,4 +1,5 @@
 package com.arithmeticTest.java;
+import java.util.Scanner;
 /*
 一球从h米高度自由落下，
 每次落地后反弹回原高度的一半再落下，
@@ -6,5 +7,14 @@ package com.arithmeticTest.java;
 第n次反弹多高？
  */
 public class test_13 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        float h = scanner.nextFloat();  //下落的高度
+        int n = scanner.nextInt();  //回弹的次数
 
+        double sum = 0;
+
+
+        System.out.println(String.format("%.3f", h)+" "+String.format("%.3f", sum));
+    }
 }
