72c88ef1ecfa36d15cce2fd6295124ecf798d5ac
pgwstr <2712278005@qq.com>
2022-07-21 19:38:44 +0800

    算法练习

---
+++ test_Arithmetic/src/com/arithmeticTest/java/test_19.java
@@ -0,0 +1,36 @@
+package com.arithmeticTest.java;
+import java.util.Scanner;
+/*
+修改Data类的定义
+ */
+public class test_19 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        while (scanner.hasNextInt()) {
+            int x = scanner.nextInt();
+            int y = scanner.nextInt();
+            Data data = new Data(x,y);
+            System.out.println(data.getX() + data.getY());
+        }
+    }
+
+}
+
+class Data {
+
+    private int x;
+    private int y;
+
+    public Data(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+}
