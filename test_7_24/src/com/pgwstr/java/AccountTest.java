0c529e0c6dca25a4472eea9a84bbfc3e1dd4548c
pgwstr <2712278005@qq.com>
2022-07-24 23:16:32 +0800

    算法练习

--- test_7_24/src/com/pgwstr/java/AccountTest.java
+++ test_7_24/src/com/pgwstr/java/AccountTest.java
@@ -4,12 +4,13 @@ public class AccountTest {
     public static void main(String[] args) {
         Account b1 = new Account(1122,20000,0.045);
         b1.Withdraw(30000);
-        System.out.println(b1.getBalance());
+        System.out.println("您的帐户余额为："+b1.getBalance());
 
         b1.Withdraw(2500);
-        System.out.println(b1.getBalance());
+        System.out.println("您的帐户余额为："+b1.getBalance());
 
         b1.deposit(3000);
-        System.out.println(b1.getBalance());
+        System.out.println("您的帐户余额为："+b1.getBalance());
+        System.out.println("您的月利率为："+(b1.getMonthlyInterest() * 100)+"%");
     }
 }
