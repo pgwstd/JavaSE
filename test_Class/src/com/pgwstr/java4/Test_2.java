00d1ae8bbfff29e846aa12ee3d2f8795bba9a17b
pgwstr <2712278005@qq.com>
2022-07-29 22:48:44 +0800

    算法练习

---
+++ test_Class/src/com/pgwstr/java4/Test_2.java
@@ -0,0 +1,12 @@
+package com.pgwstr.java4;
+
+/**
+ * @author pgwstr
+ * @date 2022/7/29 10:34 PM
+ */
+
+public class Test_2 {
+    public static void main(String[] args) {
+        System.out.println("124");
+    }
+}
---
+++ test_Class/src/com/pgwstr/java5/test.java
@@ -0,0 +1,22 @@
+package com.pgwstr.java5;
+
+/**
+ * @author pgwstr
+ * @date 2022/7/29 10:37 PM
+ */
+
+public class test {
+    public static void main(String[] args) {
+        System.out.println("12342");
+    }
+    /**
+     * @author pgwstr
+     * @date 2022/7/29 10:47 PM
+     */
+    
+    class eat{
+        public void seelp(){
+            System.out.println("睡觉");
+        }
+    }
+}
