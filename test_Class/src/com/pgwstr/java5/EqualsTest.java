4e715be6c75aed3416819b393dbbc1467c987bc4
pgwstr <2712278005@qq.com>
2022-07-30 19:32:02 +0800

    算法练习

--- test_Class/src/com/pgwstr/java5/EqualsTest.java
+++ test_Class/src/com/pgwstr/java5/EqualsTest.java
@@ -1,5 +1,7 @@
 package com.pgwstr.java5;
 
+import java.util.Date;
+
 /**
  * @author 潘光伟
  * @date 2022/7/30 6:43 PM
@@ -10,9 +12,9 @@ public class EqualsTest {
         int i = 10;
         char c = 10;
         double d = 10;
-        System.out.println(i == c);
-        System.out.println(i == d);
-        System.out.println(c == d);
+//        System.out.println(i == c);
+//        System.out.println(i == d);
+//        System.out.println(c == d);
 
         Customer t1 = new Customer("Tom",21);
         Customer t2 = new Customer("Tom",21);
@@ -23,7 +25,17 @@ public class EqualsTest {
         String s2 = new String("abc");
         System.out.println(s1 == s2);
         System.out.println(s1.equals(s2));
+        System.out.println("************");
+        Date d1 = new Date(123323l);
+        Date d2 = new Date(123323l);
+        System.out.println(d1.equals(d2));
     }
+    
+    /**
+     * @author 潘光伟
+     * @date 2022/7/30 7:31 PM
+     */
+    
 static class  Customer{
         private String name;
         private int age;
@@ -33,6 +45,25 @@ static class  Customer{
         public Customer(String name,int age){
             this.name = name;
             this.age = age;
+        }
+        public boolean equals(Object o){
+            //如果两个地址相对就直接返回true
+                if (this == o){
+                    return true;
+                }
+                //如果地址不同就比较他俩的元素是否相同
+                //看是0是否Cutomer的父类
+                if (o instanceof Customer){
+                    Customer c1 = (Customer) o;
+                    //比较这两个元素的内容
+                    if (this.age == c1.age && this.name.equals(c1.name)){
+                        return true;
+                    }
+                    else{
+                        return false;
+                    }
+                }
+                return false;
         }
 }
 }
