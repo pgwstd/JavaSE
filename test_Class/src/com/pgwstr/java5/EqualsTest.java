f6156f695282d183587572376df68bab52641cb4
pgwstr <2712278005@qq.com>
2022-07-31 21:36:45 +0800

    算法练习

--- test_Class/src/com/pgwstr/java5/EqualsTest.java
+++ test_Class/src/com/pgwstr/java5/EqualsTest.java
@@ -1,5 +1,7 @@
 package com.pgwstr.java5;
 
+import org.junit.jupiter.api.Test;
+
 import java.util.Date;
 
 /**
@@ -8,6 +10,7 @@ import java.util.Date;
  */
 
 public class EqualsTest {
+    @Test
     public static void main(String[] args) {
 //        int i = 10;
 //        char c = 10;
--- test_Class/src/com/pgwstr/java5/EqualsTestTest.java
+++ test_Class/src/com/pgwstr/java5/EqualsTestTest.java
@@ -1,6 +1,6 @@
 package com.pgwstr.java5;
 
-import java.util.Date;
+import org.junit.jupiter.api.Test;
 
 /**
  * @author 潘光伟
@@ -8,21 +8,25 @@ import java.util.Date;
  */
 
 public class EqualsTestTest {
-    public static void main(String[] args) {
+        @Test
+        public void tetEqusls(){
+            String s1 = "MM";
+            String s2 = "MM";
+            System.out.println(s1 == s2);
+        }
 
-        EqualsTest.Customer t1 = new EqualsTest.Customer("Tom", 21);
-        EqualsTest.Customer t2 = new EqualsTest.Customer("Tom", 21);
+        @Test
+    public void Equslst(){
+            EqualsTest.Customer t1 = new EqualsTest.Customer("Tom",21);
+        EqualsTest.Customer t2 = new EqualsTest.Customer("Tom",21);
         System.out.println(t1 == t2);//false
         System.out.println(t1.equals(t2));//false-->true
-        System.out.println("*************");
-        String s1 = new String("abc");
-        String s2 = new String("abc");
-        System.out.println(s1 == s2);//false
-        System.out.println(s1.equals(s2));//true
-        System.out.println("************");
-        Date d1 = new Date(123323l);
-        Date d2 = new Date(123323l);
-        System.out.println(d1.equals(d2));//true
+        }
 
-    }
+        @Test
+    public void testtoString(){
+            String s1 = new String("213123");
+            System.out.println(s1.toString());
+        }
+
 }
