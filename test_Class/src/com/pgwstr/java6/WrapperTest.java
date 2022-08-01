f2b11b9caf18fdf6b7c25328594bb28dbe7cb25a
pgwstr <2712278005@qq.com>
2022-08-01 11:56:06 +0800

    算法练习

--- test_Class/src/com/pgwstr/java6/WrapperTest.java
+++ test_Class/src/com/pgwstr/java6/WrapperTest.java
@@ -14,6 +14,12 @@ public class WrapperTest {
 //        System.out.println(num.toString());
         Integer i = new Integer(num);
         System.out.println(i.toString());
+
+        Integer i2 = new Integer("123");
+        System.out.println(i2.toString());
+
+//        Integer i3 = new Integer("123abc");
+//        System.out.println(i3.toString());
     }
 
     @Test
@@ -21,6 +27,11 @@ public class WrapperTest {
         int count = 101;
         Float f = new Float(count);
         System.out.println(f.toString());
+
+        Float f2 = new Float(3.123);
+        Float f3 = new Float(4.5);
+        System.out.println(f2);
+        System.out.println(f3.toString());
     }
 
 
