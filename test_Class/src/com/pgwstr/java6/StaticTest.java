bb31c249127ce0dc40e8033f77569226e5ee910b
pgwstr <2712278005@qq.com>
2022-08-02 12:11:58 +0800

    算法练习

--- test_Class/src/com/pgwstr/java6/StaticTest.java
+++ test_Class/src/com/pgwstr/java6/StaticTest.java
@@ -1,12 +1,30 @@
 package com.pgwstr.java6;
 
+import org.junit.jupiter.api.Test;
+
 /**
  * @author pgwstr
  * @date 2022/8/2 12:01 PM
  */
 
 public class StaticTest {
-    public static void main(String[] args) {
+    @Test
+    public void test_2(){
+
+//        Person.age = 21;
+        Person p1 = new Person();
+        p1.age = 21;
+        p1.site = "南宁";
+        System.out.println(p1.site);
+        p1.site = "玉林";
+        System.out.println(p1.site);
+        Person p2 = new Person();
+        System.out.println(p2.site);
+    }
+
+
+    @Test
+    public  void test_1() {
         Person.site = "北海";
         Person p = new Person();
         p.name = "张三";
