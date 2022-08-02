336f721906e944caddbf6fe518372a48e16214ae
pgwstr <2712278005@qq.com>
2022-08-02 22:44:53 +0800

    算法练习

--- test_Class/src/com/pgwstr/java7/Test.java
+++ test_Class/src/com/pgwstr/java7/Test.java
@@ -8,8 +8,19 @@ package com.pgwstr.java7;
 public class Test {
     public static void main(String[] args) {
         BankAccount b1 = new BankAccount();
-        BankAccount b2 = new BankAccount();
-        BankAccount b3 = new BankAccount();
-        System.out.println();
+        BankAccount b2 = new BankAccount("000000",3131);
+        BankAccount b3 = new BankAccount("666666",6666);
+        System.out.println(b1.getId());
+        System.out.println(b1.getPwd());
+        System.out.println(b1.getDeposit());
+        System.out.println("*************");
+        System.out.println(b2.getId());
+        System.out.println(b2.getPwd());
+        System.out.println(b2.getDeposit());
+        System.out.println("*************");
+        System.out.println(b3.getId());
+        System.out.println(b3.getPwd());
+        System.out.println(b3.getDeposit());
+
     }
 }
