a9a054f7a1f7fdc5fc217d4db82a025ca14ecf71
pgwstr <2712278005@qq.com>
2022-08-04 22:51:35 +0800

    算法练习

---
+++ test_Arithmetic/src/com/arithmeticTest/java/test_28.java
@@ -0,0 +1,33 @@
+package com.arithmeticTest.java;
+
+/**
+ * @author pgwstr
+ * @date 2022/8/4 10:50 PM
+ */
+
+public class test_28 {
+    public static void main(String[] args) {
+        Singleton s1 = Singleton.getInstance();
+        Singleton s2 = Singleton.getInstance();
+        System.out.println(s1 == s2);
+    }
+
+}
+
+class Singleton {
+
+    private static Singleton instance;
+
+    private Singleton() {
+
+    }
+
+    //write your code here......
+    public static Singleton getInstance() {
+        if (instance == null) {
+            instance = new Singleton();
+        }
+        return instance;
+    }
+
+}
