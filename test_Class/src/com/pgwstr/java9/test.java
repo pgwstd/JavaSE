7ba0e380e4703b233a24a99b3a2d3ca96ebf7776
pgwstr <2712278005@qq.com>
2022-08-07 17:50:00 +0800

    算法练习

--- test_Class/src/com/pgwstr/java9/test.java
+++ test_Class/src/com/pgwstr/java9/test.java
@@ -16,8 +16,14 @@ public class test {
             int n = Integer.parseInt(str);
             System.out.println("不会出现了，因为上面已经是报错不会再向下执行");
         }
+        catch (NullPointerException e){
+            System.out.println("空指针了");
+        }
         catch (NumberFormatException e){
             System.out.println("转换失败");
+        }
+        catch (Exception e){
+            System.out.println("出现异常了，请解决");
         }
     }
 }
