d72d3d48ad0cfd51a9431957ffa1da9f322ca0a7
pgwstr <2712278005@qq.com>
2022-08-10 11:37:32 +0800

    算法练习

---
+++ test_Class/src/com/pgwstr/java9/test_2.java
@@ -0,0 +1,12 @@
+package com.pgwstr.java9;
+
+/**
+ * @author pgwstr
+ * @date 2022/8/10 11:36 AM
+ */
+
+public class test_2 {
+    public static void main(String[] args) {
+        System.out.println("hello wrod");
+    }
+}
