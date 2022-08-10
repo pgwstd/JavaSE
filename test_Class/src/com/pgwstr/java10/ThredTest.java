669c767c2c85a572544f87445b40816e205b220c
pgwstr <2712278005@qq.com>
2022-08-10 17:19:37 +0800

    算法练习

---
+++ test_Class/src/com/pgwstr/java10/ThredTest.java
@@ -0,0 +1,23 @@
+package com.pgwstr.java10;
+
+/**
+ * @author pgwstr
+ * @date 2022/8/10 5:15 PM
+ */
+
+class MyThred extends Thread{
+    public void run(){
+        for (int i = 0;i < 100;i++){
+            if (i % 2 == 0){
+                System.out.println(i);
+            }
+        }
+    }
+}
+
+public class ThredTest{
+    public static void main(String[] args) {
+        MyThred m = new MyThred();
+        m.start();
+    }
+}
