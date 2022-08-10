40ee37302a67fae19277d1d030670f47f1749ec0
pgwstr <2712278005@qq.com>
2022-08-10 19:51:30 +0800

    算法练习

---
+++ test_Class/src/com/pgwstr/java10/ThredDemo.java
@@ -0,0 +1,36 @@
+package com.pgwstr.java10;
+
+/**
+ * @author pgwstr
+ * @date 2022/8/10 7:45 PM
+ */
+
+public class ThredDemo {
+    public static void main(String[] args) {
+//        odd o1 = new odd();
+//        even e1 = new even();
+//        o1.start();
+//        e1.start();
+    }
+}
+
+
+class odd extends Thread {
+    public void run() {
+        for (int i = 0; i < 100; i++) {
+            if (i % 2 == 0) {
+                System.out.println("偶数"+i);
+            }
+        }
+    }
+}
+
+class even extends Thread {
+    public void run() {
+        for (int i = 0; i < 100; i++) {
+            if (i % 2 != 0) {
+                System.out.println("奇数"+i);
+            }
+        }
+    }
+}
