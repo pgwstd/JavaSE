b53cccc944c4c7e4333f92bd2e1c0ce4df14176f
pgwstr <2712278005@qq.com>
2022-08-12 15:18:24 +0800

    算法练习

--- test_Class/src/com/pgwstr/java10/WindowTest_2.java
+++ test_Class/src/com/pgwstr/java10/WindowTest_2.java
@@ -9,21 +9,22 @@ public class WindowTest_2 {
     public static void main(String[] args) {
         Wind w1 = new Wind();
         Thread thread = new Thread(w1);
-        thread.setName("线程一");
+        thread.setName("窗口一");
 
         Thread thread2 = new Thread(w1);
-        thread2.setName("线程二");
+        thread2.setName("窗口二");
 
         Thread thread3 = new Thread(w1);
-        thread3.setName("线程三");
+        thread3.setName("窗口三");
 
         thread.start();
         thread2.start();
         thread3.start();
     }
 }
-class Wind implements Runnable{
-    private static  int ticket = 100;
+
+class Wind implements Runnable {
+    private static int ticket = 100;
 
     public void run() {
         while (true) {
