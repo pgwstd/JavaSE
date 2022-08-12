865a397683844af8a0c1418bc51e007dcadf0d52
pgwstr <2712278005@qq.com>
2022-08-12 14:47:48 +0800

    算法练习

---
+++ test_Class/src/com/pgwstr/java10/WindowTest.java
@@ -0,0 +1,38 @@
+package com.pgwstr.java10;
+
+/**
+ * @author pgwstr
+ * @date 2022/8/12 2:39 PM
+ */
+
+public class WindowTest {
+    public static void main(String[] args) {
+        Window w1 = new Window();
+        Window w2 = new Window();
+        Window w3 = new Window();
+
+        w1.setName("窗口一");
+        w2.setName("窗口二");
+        w3.setName("窗口三");
+
+        w1.start();
+        w2.start();
+        w3.start();
+    }
+}
+
+class Window extends Thread{
+    private  int ticket = 100;
+
+    public void run() {
+        while (true) {
+            if (ticket > 0) {
+                System.out.println(Thread.currentThread().getName() + "购票成功:" + ticket);
+                ticket--;
+            } else {
+                break;
+            }
+        }
+    }
+
+}
