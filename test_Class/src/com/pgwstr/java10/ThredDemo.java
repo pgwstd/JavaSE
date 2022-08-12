bd9916c58cc23d9f3a028445f1bd4c8858e247e5
pgwstr <2712278005@qq.com>
2022-08-12 10:27:02 +0800

    算法练习

--- test_Class/src/com/pgwstr/java10/ThredDemo.java
+++ test_Class/src/com/pgwstr/java10/ThredDemo.java
@@ -9,9 +9,15 @@ public class ThredDemo {
     public static void main(String[] args) {
         odd o1 = new odd();
         even e1 = new even();
-
+        o1.setName("线程一");
+        e1.setName("线程二");
         o1.start();
         e1.start();
+        Thread.currentThread().setName("主线程");
+        for (int i = 0; i < 100; i++) {
+                System.out.println(Thread.currentThread().getName()+":"+i);
+            }
+        }
 
         //匿名子类
 //        new Thread(){
@@ -35,7 +41,7 @@ public class ThredDemo {
 //            }
 //        }.start();
     }
-}
+
 
 
 class odd extends Thread {
@@ -52,7 +58,7 @@ class even extends Thread {
     public void run() {
         for (int i = 0; i < 100; i++) {
             if (i % 2 != 0) {
-                System.out.println("奇数"+i);
+                System.out.println(Thread.currentThread().getName()+":"+i);
             }
         }
     }
