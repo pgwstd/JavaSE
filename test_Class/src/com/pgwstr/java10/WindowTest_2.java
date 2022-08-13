1c9ad640ae8d4de486415c90e81515371a1c8458
pgwstr <2712278005@qq.com>
2022-08-13 17:52:51 +0800

    算法练习

--- test_Class/src/com/pgwstr/java10/WindowTest_2.java
+++ test_Class/src/com/pgwstr/java10/WindowTest_2.java
@@ -25,11 +25,11 @@ public class WindowTest_2 {
 
 class Wind implements Runnable {
     private static int ticket = 100;
-    Object obj = new Object();
+//    Object obj = new Object();
 
     public void run() {
         while (true) {
-            synchronized (obj) {
+            synchronized (this) {
                 if (ticket > 0) {
                     try {
                         Thread.sleep(100);
