25bbd723b87eb321a3bd8bb33fa64895c2fe4872
pgwstr <2712278005@qq.com>
2022-08-13 11:49:55 +0800

    算法练习

--- test_Class/src/com/pgwstr/java10/WindowTest_2.java
+++ test_Class/src/com/pgwstr/java10/WindowTest_2.java
@@ -25,20 +25,22 @@ public class WindowTest_2 {
 
 class Wind implements Runnable {
     private static int ticket = 100;
+    Object obj = new Object();
 
     public void run() {
         while (true) {
-            if (ticket > 0) {
-                try {
-                    Thread.sleep(100);
+            synchronized (obj) {
+                if (ticket > 0) {
+                    try {
+                        Thread.sleep(100);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                    System.out.println(Thread.currentThread().getName() + "购票成功:" + ticket);
+                    ticket--;
+                } else {
+                    break;
                 }
-                catch (InterruptedException e){
-                    e.printStackTrace();
-                }
-                System.out.println(Thread.currentThread().getName() + "购票成功:" + ticket);
-                ticket--;
-            } else {
-                break;
             }
         }
     }
