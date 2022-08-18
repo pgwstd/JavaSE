606f3b55721ffd7fae7df05a509b2ed480bb0bc0
pgwstr <2712278005@qq.com>
2022-08-18 23:16:27 +0800

    算法练习

--- test_Class/src/com/pgwstr/java10/WindowTest3.java
+++ test_Class/src/com/pgwstr/java10/WindowTest3.java
@@ -15,19 +15,25 @@ public class WindowTest3 {
         w1.setName("窗口一");
         w2.setName("窗口二");
         w3.setName("窗口三");
-
+        
         w1.start();
         w2.start();
         w3.start();
     }
 }
 
+
+/**
+ * @author pgwstr
+ * @date 2022/8/18 11:16 PM
+ */
+
 class Window2 extends Thread {
     private static int ticket = 100;
 
     public void run() {
         while (true) {
-
+            
             show();
 
         }
