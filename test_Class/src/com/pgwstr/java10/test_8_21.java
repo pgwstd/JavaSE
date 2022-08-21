04ba5d71d6eb4feb665ae0fe9ce3448a57d1966a
pgwstr <2712278005@qq.com>
2022-08-21 20:23:33 +0800

    算法练习

---
+++ test_Class/src/com/pgwstr/java10/test_8_21.java
@@ -0,0 +1,41 @@
+package com.pgwstr.java10;
+
+/**
+ * @author pgwstr
+ * @date 2022/8/21 8:14 PM
+ */
+
+public class test_8_21 {
+    public static void main(String[] args) {
+//        System.out.println("123");
+
+        Book b1 = new Book();
+        b1.setId(12);
+        b1.setPrc(1.23);
+
+
+        System.out.println(b1.getId());
+        System.out.println(b1.getPrc());
+    }
+
+  static   class Book{
+       private  int id;
+       private double prc;
+
+
+       public void setId(int id){
+           this.id = id;
+       }
+
+       public void setPrc(double prc){
+           this.prc = prc;
+       }
+       public int getId(){
+           return id;
+       }
+
+       public double getPrc(){
+           return prc;
+       }
+    }
+}
