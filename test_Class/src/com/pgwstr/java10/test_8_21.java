736caa056db8ac65f32808ef873b37b18a4d02a4
pgwstr <2712278005@qq.com>
2022-08-30 15:27:07 +0800

    算法练习

--- test_Class/src/com/pgwstr/java10/test_8_21.java
+++ test_Class/src/com/pgwstr/java10/test_8_21.java
@@ -7,7 +7,7 @@ package com.pgwstr.java10;
 
 public class test_8_21 {
     public static void main(String[] args) {
-//        System.out.println("123");
+        System.out.println("123");
         Book b1 = new Book();
         b1.setId(12);
         b1.setPrc(1.23);
