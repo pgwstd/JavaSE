7e6657d2dfca45724bfae57a4c6d10252d5e0e13
pgwstr <2712278005@qq.com>
2022-08-31 21:05:18 +0800

    算法练习

--- test_Class/src/com/pgwstr/java11/PordesTest.java
+++ test_Class/src/com/pgwstr/java11/PordesTest.java
@@ -10,24 +10,26 @@ class Clerk{
     private int producCont = 0;
 
     //生产产品
-    public void produceProduct() {
+    public void produceProduct() throws InterruptedException {
         if (producCont < 20){
             producCont++;
             System.out.println(Thread.currentThread().getName()+":开始生产第"+producCont+"产品");
         }
         else {
-
+            //等待
+            wait();
         }
     }
 
     //消费产品
-    public void consumeProduct() {
+    public void consumeProduct() throws InterruptedException {
         if (producCont > 0){
             System.out.println(Thread.currentThread().getName()+":开始消费第"+producCont+"产品");
             producCont--;
         }
         else {
-
+            //等待
+            wait();
         }
     }
 }
@@ -61,35 +63,37 @@ class Producer extends Thread {
  * @date 2022/8/31 20:48
  */
 //消费者
-class Consumer extends Thread{
-    private  Clerk clerk;
+class Consumer extends Thread {
+    private Clerk clerk;
 
     public Consumer(Clerk clerk) {
         this.clerk = clerk;
     }
-    
-    public void run(){
-    System.out.println(Thread.currentThread().getName()+":开始消费");
-        while (true){
-        try {
-            Thread.sleep(100);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
+
+    public void run() {
+        System.out.println(Thread.currentThread().getName() + ":开始消费");
+        while (true) {
+            try {
+                Thread.sleep(100);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            clerk.consumeProduct();
         }
-        clerk.consumeProduct();
     }
-}
-public class PordesTest {
-    public static void main(String[] args) {
-        Clerk clerk = new Clerk();
-        Producer p1 = new Producer(clerk);//生产者
-        p1.setName("生产者");
 
-        Consumer c1 = new Consumer(clerk);//消费者
-        c1.setName("消费者");
+    public class PordesTest {
+        public static void main(String[] args) {
+            Clerk clerk = new Clerk();
+            Producer p1 = new Producer(clerk);//生产者
+            p1.setName("生产者");
+
+            Consumer c1 = new Consumer(clerk);//消费者
+            c1.setName("消费者");
 
 
-        p1.start();
-        c1.start();
+            p1.start();
+            c1.start();
+        }
     }
 }
