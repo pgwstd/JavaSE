adb360e9b8032f21cb3ac54e22fc6b98ccb26001
pgwstr <2712278005@qq.com>
2022-08-31 20:51:35 +0800

    算法练习

--- test_Class/src/com/pgwstr/java11/PordesTest.java
+++ test_Class/src/com/pgwstr/java11/PordesTest.java
@@ -4,6 +4,7 @@ package com.pgwstr.java11;
  * @author pgwstr
  * @date 2022/8/31 20:45
  */
+//店员
 class Clerk{
 
 }
@@ -35,6 +36,12 @@ class Consumer extends Thread{
 }
 public class PordesTest {
     public static void main(String[] args) {
+        Clerk clerk = new Clerk();
+        Producer p1 = new Producer(clerk);//生产者
+        p1.setName("生产者");
+
+        Consumer c1 = new Consumer(clerk);//消费者
+        c1.setName("消费者");
 
     }
 }
