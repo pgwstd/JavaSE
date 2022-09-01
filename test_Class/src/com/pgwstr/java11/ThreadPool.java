26b59c064a9408c4c98e277775bb35d7e3751188
pgwstr <2712278005@qq.com>
2022-09-01 22:58:21 +0800

    算法练习

---
+++ test_Class/src/com/pgwstr/java11/ThreadPool.java
@@ -0,0 +1,24 @@
+package com.pgwstr.java11;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+/**
+ * @author pgwstr
+ * @date 2022/9/1 22:54
+ */
+class Num implements Runnable{
+    public void run(){
+        for (int i = 0;i <= 100;i++){
+            if (i % 2 ==0){
+                System.out.println(Thread.currentThread().getName()+":"+i);
+            }
+        }
+    }
+}
+public class ThreadPool {
+    public static void main(String[] args) {
+        ExecutorService service = Executors.newFixedThreadPool(10);
+        service.execute(new Num());
+    }
+}
