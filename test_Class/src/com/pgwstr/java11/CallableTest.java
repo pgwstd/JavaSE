ab5603584546854926f9d3f75d479118237fdbaa
pgwstr <2712278005@qq.com>
2022-09-01 22:08:07 +0800

    算法练习

---
+++ test_Class/src/com/pgwstr/java11/CallableTest.java
@@ -0,0 +1,31 @@
+package com.pgwstr.java11;
+
+import java.util.concurrent.Callable;
+
+/**
+ * @author pgwstr
+ * @date 2022/9/1 22:03
+ */
+
+
+class NumThread implements Callable {
+
+
+    public Object call() throws Exception{
+        int sum = 0;
+        for (int i = 0;i <= 100;i++){
+            if (i % 2 == 0){
+                System.out.println(i);
+                sum += i;
+            }
+        }
+        return sum;
+    }
+}
+
+
+public class CallableTest {
+    public static void main(String[] args) {
+
+    }
+}
