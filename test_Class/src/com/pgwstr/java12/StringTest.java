15e36b426fe7a645df3dc6c65e491803abaa0f06
pgwstr <2712278005@qq.com>
2022-09-03 21:09:35 +0800

    算法练习

--- test_Class/src/com/pgwstr/java12/StringTest.java
+++ test_Class/src/com/pgwstr/java12/StringTest.java
@@ -19,10 +19,14 @@ public class StringTest {
         System.out.println(s1 == s2);
         System.out.println(s1);
         System.out.println(s2);
+
         System.out.println("*************");
         String s3 = "abc";
         s3 += "def";
         System.out.println(s3);
         System.out.println(s2);
+
+        System.out.println("*************");
+
     }
 }
