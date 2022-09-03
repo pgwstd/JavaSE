461d3dc93ef12417ffc16eb2d3a99a89585919ed
pgwstr <2712278005@qq.com>
2022-09-03 19:22:15 +0800

    算法练习

---
+++ test_Arithmetic/src/com/arithmeticTest/java/test_29.java
@@ -0,0 +1,20 @@
+package com.arithmeticTest.java;
+
+import java.util.Scanner;
+
+/**
+ * @author pgwstr
+ * @date 2022/9/3 19:13
+ * 动态字符串
+ */
+
+public class test_29 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String  str = scanner.next();
+
+    }
+    class StrSpe{
+
+    }
+}
