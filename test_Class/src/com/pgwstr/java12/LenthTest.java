222446118bd112e5405086771ffe25de9f254837
pgwstr <2712278005@qq.com>
2022-09-04 16:02:08 +0800

    算法练习

--- test_Class/src/com/pgwstr/java12/LenthTest.java
+++ test_Class/src/com/pgwstr/java12/LenthTest.java
@@ -69,7 +69,7 @@ public class LenthTest {
     }
     @Test
     public void test4(){
-        String s1 = "abcdef";
+        String s1 = "abcadef";
         String s2 = s1.replace("a", "b");//替换某个字符
         System.out.println(s1);
         System.out.println(s2);
