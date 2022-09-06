713525968fcdd17e322dd4ebbc1bbecfb2479799
pgwstr <2712278005@qq.com>
2022-09-06 22:34:38 +0800

    算法练习

--- test_Arithmetic/src/com/arithmeticTest/java/ReverseTest.java
+++ test_Arithmetic/src/com/arithmeticTest/java/ReverseTest.java
@@ -18,6 +18,12 @@ public class ReverseTest {
         System.out.println(str3);
         System.out.println(str);
     }
+    @Test
+    public void test2(){
+        String str = "abcdefg";
+        StringTest s1 = new StringTest();
+        System.out.println(s1.recerse1(str, 2, 4));
+    }
 /**
  * @author pgwstr
  * @date 2022/9/6 14:29
@@ -28,6 +34,12 @@ public class ReverseTest {
 
         public StringTest() {
         }
+        /**
+         * @author pgwstr
+         * @date 2022/9/6 22:23
+         * 方式一
+         */
+
         public String recerse(String str,int startIndex,int endIndex){
             if (str != null){
                 char[] arr = str.toCharArray();
@@ -41,6 +53,17 @@ public class ReverseTest {
         return  null;
         }
 
+    public String recerse1(String str,int startIndex,int endIndex) {
+        if (str != null) {      //当字符串为空时，返回null
+            String recerseStr = str.substring(0, startIndex);//保存从0到启始位置(左开右闭)
+            for (int i = endIndex; i >= startIndex; i--) {     //从最后一个位置把字符一个个放在recerseStr中
+                recerseStr += str.charAt(i);
+            }
+            recerseStr += str.substring(endIndex + 1);//把后面没放在recerseStr放在其中
+            return recerseStr;
+        }
+        return null;
+    }
     }
 
 }
