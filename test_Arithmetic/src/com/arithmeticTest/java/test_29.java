bba7e499078a1c6c5384e502c9b9d534fbce3ef3
pgwstr <2712278005@qq.com>
2022-09-06 13:00:34 +0800

    算法练习

--- test_Arithmetic/src/com/arithmeticTest/java/test_29.java
+++ test_Arithmetic/src/com/arithmeticTest/java/test_29.java
@@ -8,10 +8,17 @@ package com.arithmeticTest.java;
 
 public class test_29 {
     public static void main(String[] args) {
-
+        String s1 = " abc dfa fad ";
+        System.out.println(s1);
+        System.out.println("---" + s1.trim() + "---");
 
     }
-    class TrimTest{
-        
+
+    class TrimTest {
+//        public String trimTT(){
+//
+//            return ;
+//
+//        }
     }
 }
