4d77304cba4cc223ff5801b9a6ab38ae14533f5b
pgwstr <2712278005@qq.com>
2022-09-07 21:42:49 +0800

    算法练习

--- test_Arithmetic/src/com/arithmeticTest/java/GetCount.java
+++ test_Arithmetic/src/com/arithmeticTest/java/GetCount.java
@@ -25,7 +25,7 @@ public class GetCount {
             int count = 0;//新建计数器
             int index = 0;//新建开始找的位置
             if (strLength >= findLength) {//判断原字符的长度是否大于等于被查找的字符的长度，如果不是则返回0
-                while ((index = str.indexOf(find)) != -1) {//循环，如果查找到了,就
+                while ((index = str.indexOf(find)) != -1) {//循环，如果查找到了,就把查找到的位置号给index，如果没有查找到就等于-1
                     count++;
                     str = str.substring(index + findLength);
                 }
