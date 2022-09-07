411b57d1d512cb2201aa08c0da25b3064bcf3878
pgwstr <2712278005@qq.com>
2022-09-07 21:57:41 +0800

    算法练习

--- test_Arithmetic/src/com/arithmeticTest/java/StringDemo2.java
+++ test_Arithmetic/src/com/arithmeticTest/java/StringDemo2.java
@@ -1,9 +1,21 @@
 package com.arithmeticTest.java;
 
+import org.junit.Test;
+
 /**
  * @author pgwstr
  * @date 2022/9/7 21:54
+ * 获取两个字符串中最大
  */
 
 public class StringDemo2 {
+    @Test
+    public void test() {
+
+    }
+
+    class MaxStr{
+
+    }
+
 }
