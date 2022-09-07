74ef4cc49198beabf82126e3a2d7b1bc0c5714fd
pgwstr <2712278005@qq.com>
2022-09-07 18:58:19 +0800

    算法练习

---
+++ test_Arithmetic/src/com/arithmeticTest/java/GetCount.java
@@ -0,0 +1,22 @@
+package com.arithmeticTest.java;
+
+import org.junit.Test;
+
+/**
+ * @author pgwstr
+ * @date 2022/9/7 18:51
+ * 获取一个字符串在另外一个字符串中出现的次数
+ */
+
+public class GetCount {
+    @Test
+    public void test() {
+
+    }
+   class StrDemo{
+        public int getCont(String str,String find){
+
+            return 1;
+        }
+   }
+}
