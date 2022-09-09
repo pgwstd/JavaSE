f05ccf99ae64d7a8ce658c845c63e636b4c8d698
pgwstr <2712278005@qq.com>
2022-09-09 22:53:45 +0800

    算法练习

--- test_Arithmetic/src/com/arithmeticTest/java/StringDemo2.java
+++ test_Arithmetic/src/com/arithmeticTest/java/StringDemo2.java
@@ -21,9 +21,9 @@ public class StringDemo2 {
     class MaxStr {
         public String MaxString(String str, String str2) {
             if (str != null && str2 != null) {//判断两个字符串是否有一个为空，如果有一个为空就返回null
-                String Maxstr = str.length() >= str2.length() ? str : str2;
-                String Minstr = str.length() < str2.length() ? str : str2;
-                int length = Minstr.length();
+                String Maxstr = str.length() >= str2.length() ? str : str2;//获取这两个字符串哪个是最长的
+                String Minstr = str.length() < str2.length() ? str : str2;//获取这两个字符串哪个是最短的
+                int length = Minstr.length();//获取最短字符的长度
 
                 for (int i = 0; i < length; i++) {
                     for (int j = 0, z = length - i; z <= length; j++, z++) {
