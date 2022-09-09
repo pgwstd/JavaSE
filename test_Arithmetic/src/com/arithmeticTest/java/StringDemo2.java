22aa8c28eca1fbcc42bc2dd78ffe222bb393eadf
pgwstr <2712278005@qq.com>
2022-09-09 22:57:34 +0800

    算法练习

--- test_Arithmetic/src/com/arithmeticTest/java/StringDemo2.java
+++ test_Arithmetic/src/com/arithmeticTest/java/StringDemo2.java
@@ -23,10 +23,10 @@ public class StringDemo2 {
             if (str != null && str2 != null) {//判断两个字符串是否有一个为空，如果有一个为空就返回null
                 String Maxstr = str.length() >= str2.length() ? str : str2;//获取这两个字符串哪个是最长的
                 String Minstr = str.length() < str2.length() ? str : str2;//获取这两个字符串哪个是最短的
-                int length = Minstr.length();//获取最短字符的长度
+                int length = Minstr.length();//获取最短字符的长度，来确定要查找的次数，和查找的对象
 
-                for (int i = 0; i < length; i++) {
-                    for (int j = 0, z = length - i; z <= length; j++, z++) {
+                for (int i = 0; i < length; i++) {//查找最小长度的字符串的次数，如果查找完最小字符串的长度还没有找到，证明里面没有以之相同的字符串
+                    for (int j = 0, z = length - i; z <= length; j++, z++) {//
                         String subStr = Minstr.substring(j, z);
                         if (Maxstr.contains(subStr)) {
                             return subStr;
