ba22c9a34adb79af80182e79b103d915eba99e9b
pgwstr <2712278005@qq.com>
2022-09-10 10:20:17 +0800

    算法练习

--- test_Class/src/com/pgwstr/java12/DateTest.java
+++ test_Class/src/com/pgwstr/java12/DateTest.java
@@ -2,6 +2,10 @@ package com.pgwstr.java12;
 
 import org.junit.jupiter.api.Test;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+
 /**
  * @author pgwstr
  * @date 2022/9/10 10:13
@@ -13,6 +17,13 @@ public class DateTest {
         /*
         localTime loclaData localDateTime测试
          */
+        LocalDate localDate = LocalDate.now();//获取当前日期
+        System.out.println(localDate);
 
+        LocalTime localTime = LocalTime.now();
+        System.out.println(localTime);//获取当前时间
+
+        LocalDateTime localDateTime = LocalDateTime.now();//获取当前日期与时间
+        System.out.println(localDateTime);
     }
 }
