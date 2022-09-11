b2e0b41c0d55501e69a2aa7653c96ce795f4bedb
pgwstr <2712278005@qq.com>
2022-09-12 00:15:17 +0800

    算法练习

---
+++ test_Class/src/com/pgwstr/java14/CompareTest.java
@@ -0,0 +1,15 @@
+package com.pgwstr.java14;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * @author pgwstr
+ * @date 2022/9/12 00:14
+ */
+
+public class CompareTest {
+    @Test
+    public void test(){
+
+    }
+}
