dcd7092101e75c2044d967a4bf1cb35aee7cfc85
pgwstr <2712278005@qq.com>
2022-09-12 00:22:05 +0800

    算法练习

--- test_Class/src/com/pgwstr/java14/CompareTest.java
+++ test_Class/src/com/pgwstr/java14/CompareTest.java
@@ -2,6 +2,8 @@ package com.pgwstr.java14;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.Arrays;
+
 /**
  * @author pgwstr
  * @date 2022/9/12 00:14
@@ -10,6 +12,8 @@ import org.junit.jupiter.api.Test;
 public class CompareTest {
     @Test
     public void test(){
-
+        String[] arr = new String[]{"AA", "CC", "DD", "BB", "FF", "EE"};
+        Arrays.sort(arr);
+        System.out.println(Arrays.toString(arr));
     }
 }
