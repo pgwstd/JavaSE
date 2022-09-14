4463f7e4139b5755571a2c805eee8776fed41e3a
pgwstr <2712278005@qq.com>
2022-09-14 18:54:47 +0800

    算法练习

--- test_Class/src/com/pgwstr/java14/OtherClassTest.java
+++ test_Class/src/com/pgwstr/java14/OtherClassTest.java
@@ -2,6 +2,10 @@ package com.pgwstr.java14;
 
 import org.junit.jupiter.api.Test;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 /**
  * @author pgwstr
  * @date 2022/9/14 18:33
@@ -12,5 +16,14 @@ public class OtherClassTest {
     public void test(){
         String property = System.getProperty("java.version");
         System.out.println(property);
+
+    }
+
+    @Test
+    public void test3() throws ParseException {
+        //方式一：java8之前
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+        java.util.Date parse = sdf.parse("2017-08-16");
+        System.out.println(parse.toString());
     }
 }
