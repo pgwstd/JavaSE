64ad06ea70a5fff1e24fc918f45d93f19ca69d27
pgwstr <2712278005@qq.com>
2022-09-16 22:48:36 +0800

    算法练习

--- test_Class/src/com/pgwstr/java14/EnumTest.java
+++ test_Class/src/com/pgwstr/java14/EnumTest.java
@@ -7,11 +7,30 @@ package com.pgwstr.java14;
 
 public class EnumTest {
     public static void main(String[] args) {
-
+        Season summer = Season.SUMMER;
+        System.out.println(summer);
 
     }
-    enum sertest{
 
+    enum Season {
+        SUMMER("夏天"),
+        WINTER("冬天");
+        private final String seasonName;
+
+        private Season(String seasonName) {
+            this.seasonName = seasonName;
+        }
+
+        public String getSeasonName() {
+            return seasonName;
+        }
+
+        @Override
+        public String toString() {
+            return "Season{" +
+                    "seasonName='" + seasonName + '\'' +
+                    '}';
+        }
     }
 
 }
