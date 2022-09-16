0000bc42a0c759bda3e9c2c70593cecd365f2ba9
pgwstr <2712278005@qq.com>
2022-09-16 22:40:05 +0800

    算法练习

---
+++ test_Class/src/com/pgwstr/java14/EnumTest.java
@@ -0,0 +1,17 @@
+package com.pgwstr.java14;
+
+/**
+ * @author pgwstr
+ * @date 2022/9/16 22:39
+ */
+
+public class EnumTest {
+    public static void main(String[] args) {
+
+
+    }
+    enum sertest{
+
+    }
+
+}
