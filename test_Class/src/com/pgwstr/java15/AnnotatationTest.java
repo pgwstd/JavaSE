5bb4dfcafce9e8c6ab314e4ca29b2299aea45e71
pgwstr <2712278005@qq.com>
2022-09-17 21:16:54 +0800

    算法练习

--- test_Class/src/com/pgwstr/java15/AnnotatationTest.java
+++ test_Class/src/com/pgwstr/java15/AnnotatationTest.java
@@ -1,5 +1,11 @@
 package com.pgwstr.java15;
 
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Date;
+
 /**
  * @author pgwstr
  * @date 2022/9/17 13:18
@@ -7,5 +13,23 @@ package com.pgwstr.java15;
  */
 
 public class AnnotatationTest {
+        @Test
+    public void test(){
+            Collection coll = new ArrayList();
+            coll.add("aa");
+            coll.add("bb");
+            coll.add(123);
+            coll.add(new Date());
+            System.out.println(coll.size());
 
+            Collection coll1 = new ArrayList();
+            coll1.add("cc");
+            coll1.add(456);
+            coll.add(coll1);
+            System.out.println(coll1.size());
+
+            System.out.println(coll.isEmpty());
+            coll.clear();
+            System.out.println(coll.isEmpty());
+        }
 }
