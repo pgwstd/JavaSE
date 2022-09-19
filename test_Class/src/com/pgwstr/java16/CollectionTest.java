2fe8a16700ecfa1f8913be50bb401ff740dcc8a9
pgwstr <2712278005@qq.com>
2022-09-19 18:17:33 +0800

    算法练习

--- test_Class/src/com/pgwstr/java16/CollectionTest.java
+++ test_Class/src/com/pgwstr/java16/CollectionTest.java
@@ -1,9 +1,23 @@
 package com.pgwstr.java16;
 
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
 /**
  * @author pgwstr
  * @date 2022/9/19 18:09
  */
 
 public class CollectionTest {
+    @Test
+    public void test() {
+        Collection collection = new ArrayList();
+        collection.add(123);
+        collection.add(456);
+        collection.add(new String("TOM"));
+        collection.add(false);
+        System.out.println(collection.toString());
+    }
 }
