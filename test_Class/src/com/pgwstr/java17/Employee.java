d0ce618a1aa1d581e72c7e7983f1533b2eed810e
pgwstr <2712278005@qq.com>
2022-10-03 23:02:05 +0800

    算法练习

---
+++ test_Class/src/com/pgwstr/java17/Employee.java
@@ -0,0 +1,54 @@
+package com.pgwstr.java17;
+
+/**
+ * @author pgwstr
+ * @date 2022/10/3 22:52
+ */
+
+public class Employee {
+    private String name;
+    private int age;
+    private MyDate birthday;
+
+    public Employee() {
+    }
+
+    public Employee(String name, int age, MyDate birthday) {
+        this.name = name;
+        this.age = age;
+        this.birthday = birthday;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public MyDate getBirthday() {
+        return birthday;
+    }
+
+    public void setBirthday(MyDate birthday) {
+        this.birthday = birthday;
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                ", birthday=" + birthday +
+                '}';
+    }
+}
---
+++ test_Class/src/com/pgwstr/java17/EmployeeTest.java
@@ -0,0 +1,25 @@
+package com.pgwstr.java17;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.TreeSet;
+
+/**
+ * @author pgwstr
+ * @date 2022/10/3 22:55
+ */
+
+public class EmployeeTest {
+    @Test
+    //使用自然排序
+    public void test(){
+        TreeSet treeSet = new TreeSet();
+        Employee e1 = new Employee("xiaoming",23,new MyDate(1992,1,1));
+        Employee e2 = new Employee("xiaohong",24,new MyDate(1993,8,12));
+        Employee e3 = new Employee("xiaowuan",21,new MyDate(1996,3,15));
+        Employee e4 = new Employee("xiaopan",19,new MyDate(1995,4,16));
+        Employee e5 = new Employee("xiaolou",22,new MyDate(1999,5,17));
+
+    }
+
+}
