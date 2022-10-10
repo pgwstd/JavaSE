d14b0753efe4cc99b565f8aafa68bdfbb5e64cf7
pgwstr <2712278005@qq.com>
2022-10-10 23:18:54 +0800

    练习

--- test_Class/src/com/pgwstr/java18/CollectionsTest.java
+++ test_Class/src/com/pgwstr/java18/CollectionsTest.java
@@ -2,6 +2,10 @@ package com.pgwstr.java18;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 /**
  * @author pgwstr
  * @date 2022/10/10 23:05
@@ -10,6 +14,18 @@ import org.junit.jupiter.api.Test;
 public class CollectionsTest {
     @Test
     public void test(){
+        List list = new  ArrayList();
+        list.add(123);
+        list.add(54);
+        list.add(765);
+        list.add(-90);
+        list.add(0);
+
+        Collections.reverse(list);  //反转输出
+        System.out.println(list);
+
+        Collections.shuffle(list);  //随机输出
+        System.out.println(list);
 
     }
 }
