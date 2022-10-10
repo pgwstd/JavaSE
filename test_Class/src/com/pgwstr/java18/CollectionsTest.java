25c140a7dc12507d1e47df2f59d53f1222320272
pgwstr <2712278005@qq.com>
2022-10-10 23:06:55 +0800

    练习

---
+++ test_Class/src/com/pgwstr/java18/CollectionsTest.java
@@ -0,0 +1,15 @@
+package com.pgwstr.java18;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * @author pgwstr
+ * @date 2022/10/10 23:05
+ */
+
+public class CollectionsTest {
+    @Test
+    public void test(){
+
+    }
+}
