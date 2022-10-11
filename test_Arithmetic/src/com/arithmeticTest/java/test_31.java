40081d4ba2fb94bfc02a62353bccb98e5484a914
pgwstr <2712278005@qq.com>
2022-10-11 10:25:01 +0800

    练习

---
+++ test_Arithmetic/src/com/arithmeticTest/java/test_31.java
@@ -0,0 +1,15 @@
+package com.arithmeticTest.java;
+
+/**
+ * @author pgwstr
+ * @date 2022/10/11 10:22
+ * 一只青蛙一次可以跳上1级台阶，
+ *也可以跳上2级。求该青蛙跳上一个 n 级的台阶
+ * 总共有多少种跳法（先后次序不同算不同的结果）
+ */
+
+public class test_31 {
+    public static void main(String[] args) {
+
+    }
+}
