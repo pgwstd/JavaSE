cd6400290b3b4e7c4eef8083d5a51e9459559619
pgwstr <2712278005@qq.com>
2022-10-15 23:33:49 +0800

    算法练习

--- test_Class/src/com/pgwstr/java19/ChildTow.java
+++ test_Class/src/com/pgwstr/java19/ChildTow.java
@@ -9,4 +9,17 @@ public class ChildTow extends Order<String>{
         private int BankId;
         private String BankName;
 
+    public ChildTow() {
+    }
+
+    public ChildTow(int bankId, String bankName) {
+        BankId = bankId;
+        BankName = bankName;
+    }
+
+    public ChildTow(int id, String name, String orderT, int bankId, String bankName) {
+        super(id, name, orderT);
+        BankId = bankId;
+        BankName = bankName;
+    }
 }
