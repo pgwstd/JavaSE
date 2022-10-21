1605130aba4e422184f9d0c8f7fc37f7b649e73d
pgwstr <2712278005@qq.com>
2022-10-22 00:00:28 +0800

    练习

---
+++ test_Class/src/com/pgwstr/java21/FileTest.java
@@ -0,0 +1,15 @@
+package com.pgwstr.java21;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * @author pgwstr
+ * @date 2022/10/21 23:57
+ */
+
+public class FileTest {
+    @Test
+    public void test(){
+
+    }
+}
