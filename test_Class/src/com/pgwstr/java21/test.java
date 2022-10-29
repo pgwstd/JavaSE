ab64c946737234376dfb89f137c929a03999fab3
pgwstr <2712278005@qq.com>
2022-10-29 20:05:16 +0800

    练习

---
+++ test_Class/src/com/pgwstr/java21/test.java
@@ -0,0 +1,11 @@
+package com.pgwstr.java21;
+
+import org.junit.jupiter.api.Test;
+
+public class test {
+    @Test
+    public void test1() {
+        //遍历value
+        System.out.println("test1");
+    }
+}
