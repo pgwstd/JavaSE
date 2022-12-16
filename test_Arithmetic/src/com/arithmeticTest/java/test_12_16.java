59e2dbd0815cb3fd86d09eee878fecde624f158b
panguangwei <2712278005@qq.com>
2022-12-16 21:05:07 +0800

    签到

---
+++ test_Arithmetic/src/com/arithmeticTest/java/test_12_16.java
@@ -0,0 +1,14 @@
+package com.arithmeticTest.java;
+//12.16算法练习
+public class test_12_16 {
+    public static void main(String[] args) {
+        System.out.println("zhe");
+        System.out.println("shi");
+        System.out.println("yi");
+        System.out.println("dao");
+        System.out.println("qian");
+        System.out.println("dao");
+        System.out.println("ti");
+    }
+
+}
