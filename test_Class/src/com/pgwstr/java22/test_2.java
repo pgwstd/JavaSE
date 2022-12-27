8b3780949d27a8b765084e7778a44f1771df83a7
panguangwei <2712278005@qq.com>
2022-12-27 22:07:02 +0800

    12_27

--- test_Class/src/com/pgwstr/java22/test_2.java
+++ test_Class/src/com/pgwstr/java22/test_2.java
@@ -6,6 +6,7 @@ package com.pgwstr.java22;
  */
 public class test_2 {
     public static void main(String[] args) {
+        int count = 0;
         for (int i = 2; i <= 10000; i++) {
             boolean isTrue = true;
             for (int j = 2; j < i; j++) {
@@ -16,8 +17,15 @@ public class test_2 {
 
             }
             if (isTrue) {
-                System.out.println(i);
+                count++;
+                System.out.print(i + " ");
+                if (count % 8 == 0) {
+                    System.out.println();
+                }
             }
+
         }
+        System.out.println();
+        System.out.println("1-10000一共有" + count + "个素数");
     }
 }
