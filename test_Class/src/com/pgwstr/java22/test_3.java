981a615a7d8b350453311a7651dd55ae216627d5
panguangwei <2712278005@qq.com>
2022-12-29 21:00:23 +0800

    12_29

---
+++ test_Class/src/com/pgwstr/java22/test_3.java
@@ -0,0 +1,16 @@
+package com.pgwstr.java22;
+
+/**
+ * @author pgwstr
+ * @date 2022/12/29
+ */
+public class test_3 {
+    public static void main(String[] args) {
+        System.out.println(sum(12,14));
+    }
+
+    //定义一个方法，让它可以接收两个int类型的参数，返回这两个参数的和
+    public static int sum(int a, int b) {
+        return a + b;
+    }
+}
