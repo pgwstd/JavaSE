9fedc7a0562eb26045222363e7185cd1a532a537
panguangwei <2712278005@qq.com>
2023-01-12 21:42:10 +0800

    1_12

--- test_Class/src/com/pgwstr/java22/test_3.java
+++ test_Class/src/com/pgwstr/java22/test_3.java
@@ -9,7 +9,6 @@ public class test_3 {
         System.out.println(sum(12, 14));
     }
 
-    //定义一个方法，让它可以接收两个int类型的参数，返回这两个参数的和
     public static int sum(int a, int b) {
         return a + b;
     }
